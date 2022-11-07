27ddc18ffabbaf97f4cc0859295cfa6e72da3e5f
Jesper Orb <github@jesperorb.com>
2022-11-07 20:12:01 +0100

    chore: revert back to tsc build process

--- packages/jest-reporter/src/index.ts
+++ packages/jest-reporter/src/index.ts
@@ -1,7 +1,4 @@
 import { TsDocTaggedTestReporter } from './lib/reporter';
-export { parseTestFiles } from './lib/test-file-parser';
-export { groupTests } from './lib/grouper';
-export { render } from './lib/renderer';
 export * from './lib/types';
 
 export default TsDocTaggedTestReporter;
